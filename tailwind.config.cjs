4c270beba681693b9ce0bdd88f7b083d8ea9afdd
Miguel Linares <malg20012002@gmail.com>
2023-03-03 18:06:17 -0500

    config tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
