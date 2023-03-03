75a3ca7baafcee534339844ab0252be4565043d4
Miguel Linares <malg20012002@gmail.com>
2023-03-03 16:54:29 -0500

    add component headboard

---
+++ src/components/Home/Headboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Headboard = () => {
+  return (
+    <div>
+      hola soy Home
+    </div>
+  )
+};
+
+export default Headboard;
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Headboard from "./Headboard";
+
+const Home = () => {
+  return (
+    <div>
+      <Headboard />
+    </div>
+  )
+}
+
+export default Home
